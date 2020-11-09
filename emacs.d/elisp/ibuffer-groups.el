d6e194eea6ca6e1fb817e3d6ae599091e7731470
George Whiteside <whiteside@reservoir.com>
2020-11-09 11:16:17 -0500

    Delete ibuffer-groups.el

--- emacs.d/elisp/ibuffer-groups.el
+++
@@ -1,8 +0,0 @@
-;; add example groups
-(add-to-list 'ibuffer-saved-filter-groups
-             `("example"
-               ,my-default-filter-groups
-               ("C" (mode . c-mode))))
-
-;; override my-groups to load example groups
-(setq my-groups "example")
